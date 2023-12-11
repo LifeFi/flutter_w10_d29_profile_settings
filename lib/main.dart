9c3d7f32a428348f989c147009b2ef645b4eb048
topcircle <topcircle@gmail.com>
2023-12-11 18:42:08 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_w10_d29_profile_settings/features/main_navigation/main_navigation_screen.dart';
-import 'package:flutter_w10_d29_profile_settings/features/users/views/settings_screen.dart';
 
 void main() {
   runApp(const MyApp());
